c290d0f0068043a28eeedfe6b06e0802af324874
Utkarsh Ranjan <utkarsh13ranjan@gmail.com>
2023-05-10 02:30:49 +0530

    Searching and Sorting

--- sorting-and-searching/concert-tickets.cpp
+++ sorting-and-searching/concert-tickets.cpp
@@ -1,38 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-typedef long long ll;
-const int N = 2e5+10;
-const double eps = 1e-9;
-const double pi = acos( -1 );
-const int inf = 0x3f3f3f3f;
+int n, m, h, t;
+multiset<int> tickets;
 
-ll binarySearch(vector<ll> arr, ll l, ll r, ll x, vector<ll> arr_copy)
-{
-
-	if (r >= l) {
-
-		ll mid = l + (r - l) / 2;
-
-		if (arr[mid] == x){
-			while(arr_copy[mid]==0 && mid >=0) mid--;
-			return mid;
-			}
-
-		if (arr[mid] > x){
-			return binarySearch(arr, l, mid - 1, x, arr_copy);
-		}
-
-		return binarySearch(arr, mid + 1, r, x ,arr_copy);
+void solve(){
+	cin >> n >> m ;
+	for(int i=0;i < n;++i){
+		cin >> h;
+		tickets.insert(h);
 	}
-
-	return r;
+	for(int i=0;i<m;++i){
+		cin >> t;
+		auto it = tickets.upper_bound(t);
+		if(it == tickets.begin()){
+			cout << -1 << "\n";
+		}else{
+			cout << *(--it) << "\n";
+			tickets.erase(it);
+		}
+	}
 }
 
 int main(){
-	ll n,m;cin >> n >> m;
-	vector<ll> a(n,0), b(n,0), a_copy(n); 
-	for(ll i=0;i<n;i++) cin >> a[i];
-	sort(a.begin(),a.end());
 	
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	solve();
+	return 0;
+
 }
